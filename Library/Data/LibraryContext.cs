bda71d6b25a63e6b8f484cefc574770dbefcad31
Daniel Thyselius <Daniel.Thyselius@officeitpartner.se>
2018-12-14 08:48:27 +0100

    Gjort lite saker, typ en book.cs och en IRepository

---
+++ Library/Data/LibraryContext.cs
@@ -0,0 +1,19 @@
+﻿using Library.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.Data
+{
+    public class LibraryContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\daniel.thyselius\Documents\Library.mdf;Integrated Security=True;Connect Timeout=30");
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ Library/Migrations/20181212151032_init.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Library.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    ISBN = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.ISBN);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
